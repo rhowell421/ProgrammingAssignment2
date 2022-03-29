a001ade6171bfbe10e2b5e213605046570bfe04e
Nova Jude <guldstjrna@Novas-MBP.verizon.net>
2022-03-29 11:13:13 -0400

    R Rrogramming Week 3 Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 1. Run the Source Code
+## 2. Create variable 'cm <- makeCacheMatrix()'     *View your matrix with 'cm$get()'
+## 3. Store the inverse matrix with 'cm_inver <- cm$getinv()'     *View the inverse matrix with 'cm_inver'
+## 4. Solve and store cached matrix 'cm_solved <- cacheSolve(cm)'     *View the solution with 'cm_solved'
+## 5. Check the code with 'identical(cm_inver, cm_solved)'      *Should equal true!
 
-## Write a short comment describing this function
+library(MASS)
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix(rnorm(1:20), 4, 5)) {
+  
+    inv <- NULL
+    
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() {
+          inver <- ginv(x)
+          inver %*% x
+    }
+    
+    list(set = set, 
+         get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
@@ -12,4 +34,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+    inv <- x$getinv()
+    
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setmean(inv)
+    inv
+
 }
